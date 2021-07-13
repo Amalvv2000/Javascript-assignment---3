8e64a8b4383d48e7bb20a677a4574e54fb49d512
Amalvv2000 <87015388+Amalvv2000@users.noreply.github.com>
2021-07-13 18:37:00 +0530

    Add files via upload
    Assignment 3 submitted

---
+++ Day-3 Assignment/index.js
@@ -0,0 +1,8 @@
+
+function Handle() {
+    var name = document.getElementById("input")
+    var data = name.value;
+    var dart = document.getElementById("caption")
+    dart.innerHTML ="The Entered Name is : " + data;
+
+}
